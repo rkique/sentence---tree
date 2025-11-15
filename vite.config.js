73066f74c7c1c7611e50d4d32315775943878f50
Eric Xia <ericpingxia@gmail.com>
2025-11-15 17:57:02 -0500

    sentence-tree config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    allowedHosts: ['urchin-app-2-mzfse.ondigitalocean.app'],
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
